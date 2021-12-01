553ff6bd6e7480d54f7233ca1fc4c7f6d0f27531
Bart Hanssens <bart.hanssens@bosa.fgov.be>
2021-12-01 22:25:16 +0100

    Initial code Signed-off-by:Bart Hanssens <bart.hanssens@bosa.fgov.be>

--- src/main/java/be/bosa/dt/rbv/Main.java
+++ src/main/java/be/bosa/dt/rbv/Main.java
@@ -30,7 +30,6 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
@@ -52,7 +51,6 @@ public class Main {
 
 	private final static RioSetting[] NON_FATAL_ERRORS = new RioSetting[] {
 			BasicParserSettings.FAIL_ON_UNKNOWN_DATATYPES,
-			BasicParserSettings.FAIL_ON_UNKNOWN_DATATYPES,
 			BasicParserSettings.FAIL_ON_UNKNOWN_LANGUAGES,			
 			BasicParserSettings.VERIFY_DATATYPE_VALUES,
 			BasicParserSettings.VERIFY_LANGUAGE_TAGS,
@@ -60,47 +58,8 @@ public class Main {
 			BasicParserSettings.VERIFY_URI_SYNTAX,
 			NTriplesParserSettings.FAIL_ON_INVALID_LINES
 	};
-/*
-	private static PipedReader[] prs;
-	private static PipedWriter[] pws;
-	private static ExecutorService executor;
 
-	private static RDFParser createValidator(int counter) throws IOException {
-		prs[counter] = new PipedReader();
-		pws[counter] = new PipedWriter();
-		prs[counter].connect(pws[counter]);
-
-		RDFParser parser = Rio.createParser(RDFFormat.NTRIPLES);
-		for (RioSetting setting: NON_FATAL_ERRORS) {
-			parser.getParserConfig().addNonFatalError(setting).set(setting, true);
-		}
-		return parser;
-	}
-
-	private static void createThreads(int cores) {
-		for (int i = 0; i < cores; i++) {
-			int core = i;
-			executor.submit(() -> {
-				try {
-					RDFParser parser = createValidator(core);
-					parser.parse(prs[core]);
-				} catch (IOException  ioe) {
-					LOG.severe(ioe.getMessage());
-				} catch (RDFParseException rpe) {
-					long nr = rpe.getLineNumber();
-					long line = (nr - 1) * cores + core + nr;
-					LOG.log(Level.WARNING, "Line: {0} {1}", new Object[]{line, rpe.getMessage()});
-				} catch (RDFHandlerException re) {
-					LOG.warning(re.getMessage());
-				}
-			});
-		}
-	}
-*/
-
-	private static void validate(String inFile, int cores) throws IOException {
-		long nr = 0;
-	
+	private static void validate(String inFile) throws IOException {
 		try(BZip2CompressorInputStream bcis = new BZip2CompressorInputStream(
 												new BufferedInputStream(new FileInputStream(inFile)));
 			BufferedReader r = new BufferedReader(new InputStreamReader(bcis))) {
@@ -110,40 +69,21 @@ public class Main {
 				parser.getParserConfig().addNonFatalError(setting).set(setting, true);
 			}
 			parser.setParseErrorListener(new ParseErrorLogger());
-			parser.parse(r);
-	/*	return parser;
-			Iterator<String> iter = r.lines().iterator();
-			while (iter.hasNext()) {
-				pws[(int)(nr % cores)].write(iter.next());
-				nr++;
-				if (nr % 100_000 == 0) {
-					LOG.log(Level.INFO, "Read {0} lines", nr);
+			parser.setParseLocationListener((long line, long col) -> {
+				if (line % 100_000 == 0) {
+					LOG.info("Line :" + line);
 				}
-			} */
+			});
+			parser.parse(r);
 		}
-		LOG.log(Level.INFO, "Read {0} lines", nr);
 	}
 	
 	public static void main(String[] args) throws IOException {
-		if (args.length < 2) {
-			System.err.println("Usage: cores compressed-nt-input-file ");
+		if (args.length < 1) {
+			System.err.println("Usage: compressed-nt-input-file ");
 		}
 
-		int cores = Integer.valueOf(args[0]);
+		validate(args[0]);
 
-	/*	prs = new PipedReader[cores];
-		pws = new PipedWriter[cores];
-
-		executor = Executors.newFixedThreadPool(cores);
-
-		createThreads(cores); */
-		validate(args[1], cores);
-
-/*		executor.shutdown();
-	
-		for (int i = 0; i < cores; i++) {
-			pws[i].close();
-			prs[i].close();
-		} */
 	} 
 }
